7585f8160d8c160c1de162e25dba517779491281
Kishan Kumar <krishankumar9943@gmail.com>
2023-07-29 15:38:44 +0530

    Logo size fix

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Loader = () => {
   return (
     <div className="h-full flex flex-col gap-y-4 items-center justify-center">
-      <div className="w-20 h-20 relative animate-spin">
+      <div className="w-12 h-12 relative animate-spin">
         <Image alt="Logo" fill src="/logo.png" />
       </div>
       <p className="text-sm text-muted-foreground">Genius is thinking</p>

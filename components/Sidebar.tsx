0351189686471134baaf6f0ec95d5da35666d849
Kishan Kumar <krishankumar9943@gmail.com>
2023-07-29 15:41:06 +0530

    Logo size fix

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -67,7 +67,7 @@ const Sidebar = () => {
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
         <Link href={"/dashboard"} className="flex items-center pl-3 mb-10">
-          <div className="relative w-10 h-10 mr-4">
+          <div className="relative w-8 h-8 mr-4">
             <Image fill alt="logo" src={"/logo.png"} />
           </div>
           <h1 className={cn("text-2xl font-bold", montserrat.className)}>
